e3d25af493de7f441d03dda97f920cbc300db4f3
Devil-Online <104246263+Devil-Online@users.noreply.github.com>
2022-04-23 23:41:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+![images (20)](https://user-images.githubusercontent.com/102848810/161901020-39219b07-13c6-402f-866f-680c6cc45358.jpeg)
+</p>
+
+# Deploy On Railway 
+
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template/LT37gJ?referralCode=HJ2ESQ)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ translation.py
@@ -0,0 +1,106 @@
+class LuciferMoringstar(object):
+
+    DEFAULT_MSG = """👋Hello {mention}.....!!!\nIt's Power Full [{bot_name}](t.me/{bot_username}) Here 😎\nAdd Me To Your Group And Make Sure I'm an Admin There! \nAnd Enjoy My Pever Show.....!!!🤪"""
+
+    HELP_MSG = """**✅ DO
+👉 Type Only In English
+
+❌ DON'T
+👉 Avoid Symbols (/.,:;'_-!)
+👉 Avoid Requesting Same Movie/Series Repeatedly
+👉 Avoid Requesting Unreleased Movie/Series**"""
+
+    ABOUT_MSG = """
+🤖 My Name [[{bot_name}]](t.me/{bot_username})
+
+🤴 My Dev : [[Allu Arjun]](https://t.me/helloalluarjunhttps://t.me/helloalluarjun)
+
+🧑‍💻 My Creator : [{dev_name}]
+
+🛠️ My Library : [Pyrogram]
+
+🈵 My Language : [Python3]
+
+🗄️ My Database : [[Mongodb]](https://mongodb.com)
+
+📡 My Server : [[Heroku]](https://heroku.com)"""
+
+    FILE_CAPTIONS = """📂 File Info - <code>{title}</code>
+✯ ━━━━━ ✧ ━━━━━ ✯
+🔘𝐉𝐨𝐢𝐧
+👉<a href=https://t.me/+1CZ0RvqgItEzZTQ1  >𝑀𝑜𝑣𝑖𝑒 𝑇𝑖𝑚𝑒 2.0</a>
+👉<a href=https://t.me/+ZgWO7gGleUgwMTc1 >𝑀𝑜𝑣𝑖𝑒 𝑇𝑖𝑚𝑒 𝑈𝑝𝑑𝑎𝑡𝑒𝑠</a>
+✯ ━━━━━ ✧ ━━━━━ ✯
+➠𝐇𝐞𝐫𝐞 𝐈𝐬 𝐘𝐨𝐮𝐫 #𝐑𝐞𝐪𝐮𝐞𝐬𝐭
+✯ ━━━━━ ✧ ━━━━━ ✯
+👋 ʜᴇʏ!!
+ᴋɪɴᴅʟʏ ᴀᴅᴅ ʏᴏᴜʀ ꜰʀɪᴇɴᴅꜱ, 
+ᴀꜰᴛᴇʀ ɢᴇᴛᴛɪɴɢ ᴍᴏᴠɪᴇꜱ/ꜱᴇʀɪᴇꜱ.
+ɪᴛ ᴡɪʟʟ ʜᴇʟᴘ ᴜꜱ ɢʀᴏᴡɪɴɢ 🙏
+✯ ━━━━━ ✧ ━━━━━ ✯
+⬆️𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲
+👉<a href='https://t.me/helloalluarjun'>𝐴𝑙𝑙𝑢 𝐴𝑟𝑗𝑢𝑛</a>
+✯ ━━━━━ ✧ ━━━━━ ✯
+𝐉𝐨𝐢𝐧🎗️ 𝐒𝐡𝐚𝐫𝐞🎗️ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭
+✯ ━━━━━ ✧ ━━━━━ ✯"""
+
+    PR0FESS0R_99 = """
+Hello👋 {mention},
+
+I Can Guide You Through All Of <a href='http://t.me/MovieTime_Robot'>Movie Time Bot</a>'s Cool Features & How To Properly Use Them. Use The Buttons Below To Navigate Through All Of The Modules."""
+
+
+ 
+ 
+ 
+
+    ADD_YOUR_GROUP = """**__Join the group I work for and request all your favorite movies in that group! Don't request here.👇__**"""
+    SPELL_CHECK = """
+**Hello 👋{mention}**,
+
+**Couldn't Find {query}?  Please Click Your Request Movie Name**"""
+    GET_MOVIE_1 = """
+**Hello 👋 {mention}**,
+
+↪️ **Requested** : [{query}]({url})
+👤 **Requested By** : {mention}
+🎬 **Title** : [{title}]({url})
+📆 **Year** : {year}
+🌟 **Rating** : {rating}/10
+🎭 **Genres** : {genres}
+🧑‍🔧 Get Support ✔️ <a href='https://t.me/helloalluarjun'>Allu Arjun</a>"""
+
+    GET_MOVIE_2 = """
+↪️ **Requested** : {query}
+👤 **Requested By** : {mention}
+🎬 Title : [{title}]({url})
+📆 Year : {year}
+🌟 Rating : {rating}/10
+🎭 Genres : {genres}
+© **{chat}**"""
+    
+    
+    WAI_MSG = """
+Hello 👋 {mention},
+I Can't Write Everything In My Bio So I Created This...
+
+╭──[─═ ᴘᴇʀꜱᴏɴᴀʟ ɪɴꜰᴏ ═─]
+├• Name⇝ Bikash Maity
+├• Gender⇝ Male
+├• TG Name⇝ Allu Arjun
+├• Username⇝ @helloalluarjun
+├• Birthday⇝ 10th January 2006
+├• Age⇝ 16
+├• Relationship Status⇝ Single
+├• Nationality⇝ Indian
+├• Location⇝ West Bengal, India
+├• Contact Bot⇝ Soon... 🤖
+╰──────⍟───────╯
+
+╭──[─═ ᴏᴡɴᴇʀ ᴀᴛ ═─]
+├• <a href='https://t.me/+1CZ0RvqgItEzZTQ1'>MOVIE TIME</a>
+├• <a href='https://t.me/+ZgWO7gGleUgwMTc1'>MOVIE TIME UPDATES</a>
+├• <a href='http://t.me/MovieTime_Robot'>MOVIE TIME BOT</a>
+╰──────⍟───────╯
+
+Thank You For Reading Patiently 🙏"""
